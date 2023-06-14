8accee0e21386bb9cf08f62b71e12d0ce6282012
bugraguler <bgrglr.be@gmail.com>
2023-06-14 16:43:13 +0200

    commit status farkı

--- src/com/classes/git/GitDenemeler.java
+++ src/com/classes/git/GitDenemeler.java
@@ -3,5 +3,6 @@ package com.classes.git;
 public class GitDenemeler {
     public static void main(String[] args) {
         System.out.println("delirmeler");
+        System.out.println("ancak sout mk" );
     }
 }

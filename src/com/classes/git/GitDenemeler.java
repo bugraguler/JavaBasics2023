e0053eaea7d18ea219233045dce62076164dcdee
bugraguler <bgrglr.be@gmail.com>
2023-06-14 16:39:09 +0200

    denemeye devam

--- src/com/classes/git/GitDenemeler.java
+++ src/com/classes/git/GitDenemeler.java
@@ -1,8 +1,7 @@
 package com.classes.git;
 
-public class GitTest {
+public class GitDenemeler {
     public static void main(String[] args) {
-        System.out.println("deneme bros" );
+        System.out.println("delirmeler");
     }
-
 }
